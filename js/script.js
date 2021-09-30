8a871d9458da8408f149b3c1bb48628a8fb5a4ce
Soph H.P <smkendall90@gmail.com>
2021-09-30 12:30:04 +0200

    removed some photos added search function

--- js/script.js
+++ js/script.js
@@ -6,3 +6,21 @@ menuButton.addEventListener("click", () => {
     ? navMenu.classList.remove("menu_open")
     : navMenu.classList.add("menu_open");
 });
+
+
+//Search
+
+const addSearchFunction = () => {
+    const searchInput = document.querySelector("#search__input");
+   searchInput.addEventListener("keyup", (e) => {
+      const searchInput = e.target.value;
+      const key = e.key;
+      if (key === "Enter" && e.target.value.length > 0) {
+        window.location.href = `/exhibitions.html?search=${searchInput}`;
+      }
+  
+      if (key === "Escape") e.target.blur();
+    });
+  };
+
+  addSearchFunction()
